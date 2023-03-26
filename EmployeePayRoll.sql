96b7fff526624f62b67a45a0cfb5c487cb9916f2
Tanvishinde03 <shindetanvi0203@gmail.com>
2023-03-26 16:03:52 +0530

    [Tanvi] Add : UC2 - Creating Table For Emp Pay Roll

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -1,2 +1,15 @@
 --UC1 
 CREATE DATABASE EmployeePayRoll_DB
+
+USE EmployeePayRoll_DB
+
+-- UC2 
+CREATE TABLE EmployeePayRoll_Table
+(
+EmployeeId INT IDENTITY(1,1) PRIMARY KEY,
+EmployeeName varchar(225),
+EmployeeSalary INT,
+StartDate Date
+);
+
+SELECT * FROM EmployeePayRoll_Table
